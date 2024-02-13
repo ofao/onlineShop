2070d8825cdadb2f9285113c0e9681e4eda88892
ofao <131997134+ofao@users.noreply.github.com>
2024-02-13 09:03:29 +0500

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<HTML>
+  <footer>
+  <div class="custom-shape-divider-bottom-1706593823">
+    <svg width="100%" id="svg" viewBox="0 0 1440 300" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 L 0,150 C 141.60000000000002,114.26666666666667 283.20000000000005,78.53333333333333 433,80 C 582.8,81.46666666666667 740.8,120.13333333333333 910,138 C 1079.2,155.86666666666667 1259.6,152.93333333333334 1440,150 L 1440,400 L 0,400 Z" stroke="none" stroke-width="0" fill="#e7ecef" fill-opacity="1" class="transition-all duration-300 ease-in-out delay-150 path-0"></path></svg>
+  </div>
+  <section class='footer-info'>
+    <section>
+      <h4>© Хачатрян О. А. 2023</h4>
+      <h4>89080586154</h4>
+      <h4>ofeliahachatryan@gmail.com</h4>
+    </section>
+    <a href="#" onclick="window.print();return false;">Распечатать</a>
+    <a href="question.html">Задать вопрос</a>
+    <a href="#" onclick="change_lang()" id='lang'>RUS</a>
+  </section>
+</footer>
+</HTML>
