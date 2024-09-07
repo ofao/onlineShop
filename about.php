be34e03d61f076c0260403942de99e4a8c157bb8
ofao <131997134+ofao@users.noreply.github.com>
2024-09-07 17:16:42 +0500

    Update and rename about.html to about.php

---
+++ about.php
@@ -0,0 +1,21 @@
+<HTML lang='ru'>
+<?php include "includes/header.php" ?>
+	<section style='background-color: #2c3e50; width: 100%; height: 100%; display: flex;'>
+		<img src='pictures/f2.jpg' width='40%' height='auto' style="max-width: 4454px; max-height: 6236px; margin: 25px">
+		<section style='height: 100%; display: flex; flex-direction: column; overflow: hidden; justify-content: center;'>
+			<h1 style='color: e7ecef; margin: 25px;'>Fashion Boutique</h1>
+			<p style='color: e7ecef; margin: 25px;'>Компания Fashion Boutique стремится к постоянному росту и развитию. Для этого компания расширяет свою сеть магазинов и развивает онлайн-магазин, чтобы достичь большего количества покупателей. Кроме того, компания активно работает над улучшением сервиса и удобства покупок.</p>
+		</section>
+	</section>
+	<section class='container'><div class='wave'></div></section>
+	<section style='width: 100%; height: 100%; display: flex;'>
+		<section style='height: 100%; display: flex; flex-direction: column; overflow: hidden; justify-content: center; background-blend-mode: screen;'>
+			<h1 style='color: #2c3e50; margin: 25px;'>rvervefvg</h1>
+			<p style='color: #2c3e50; margin: 25px;'>some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text </p>
+			<img src='pictures/11.gif' width='10%'><img src='pictures/22.gif' width='10%'><img src='pictures/3.gif' width='10%'>
+		</section>
+		<img src='pictures/f3.jpg' width='40%' height='auto' style="max-width: 4454px; max-height: 6236px; margin: 25px">
+	</section>
+<?php include "includes/footer.html" ?>
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+</HTML>
