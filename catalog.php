44bafbfb270cb344459fe91c3c6cc85997df28a4
ofao <131997134+ofao@users.noreply.github.com>
2024-09-07 17:18:42 +0500

    Update and rename catalog.html to catalog.php

---
+++ catalog.php
@@ -0,0 +1,12 @@
+<HTML>
+<link rel="stylesheet" type="text/css" href="includes/css/catalog.css">
+<?php include "includes/header.php" ?>
+	<section id='catalog' style='display: flex; flex-direction: column; justify-content: center; align-items: center;'>
+		<h1>Каталог</h1><img src='pictures/3.gif' width='5%'>
+	</section>
+	<section>
+	</section>
+</div>
+<?php include "includes/footer.html" ?>
+<script src="includes/js/catalog.js"></script>
+</HTML>
